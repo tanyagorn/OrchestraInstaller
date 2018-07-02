0b329569fd506781178cf11d20715e16a91dfb10
Tanyagorn <tanyagorn.b@gmail.com>
2018-07-02 22:29:45 +0700

    Copy JDBC driver from local to destination

---
+++ panels/src/main/java/com/izforge/izpack/panels/mypanels/FileSystem.java
@@ -0,0 +1,32 @@
+package emds.epi.impl.security;
+
+import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.*;
+
+
+import com.izforge.izpack.panels.process.AbstractUIProcessHandler;
+ 
+public class FileSystem  {
+
+  public void run(AbstractUIProcessHandler handler, String[] args)  throws Exception {
+
+		handler.logOutput("Copying JDBC driver to destination... ", false);
+		
+        Path source = Paths.get(args[0]);
+        Path destination = Paths.get(args[1]);
+        try {
+            Files.copy(source, destination.resolve(source.getFileName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
