8fb9ac3d75f32c3f1bd3dc5ae258ff024ccf28da
Tanyagorn <tanyagorn.b@gmail.com>
2018-07-12 15:10:30 +0200

    Edit env file for Unix systems

--- installer/src/main/izpack/pack_designer/Startscripts/Unix/orchestra_env.sh
+++ installer/src/main/izpack/pack_designer/Startscripts/Unix/orchestra_env.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # --------  adjust these values
-ORC_JAVA_HOME=/usr/lib/java/jre1.6.0_04
+ORC_JAVA_HOME=%JRE_PATH
 # ORC_JAVA_HOME=/usr/lib/java/jre1.7.x
-ORC_TOMCAT=/opt/apache-tomcat-5.5.26
+ORC_TOMCAT=%TOMCAT_HOME
 ORC_HOME=$ORC_TOMCAT/webapps/orchestra
 # -------- 
 
